b70faa1aae3d6229b63828697dd8ae52333757c7
iSmithmilla <jhonmilla1607@gmail.com>
2025-01-21 23:32:28 -0500

    2024.01.21 - Implementar funcion para sortear amigos - 23:32pm

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 let arrayAmigos = [];
 let inputAmigo = document.querySelector("#amigo");
 let listAmigo = document.querySelector("#listaAmigos");
+let resultadoAmigoSecreto = document.querySelector("#resultado");
+let indiceAleatorio = 0;
 let amigo = "";
 
 
@@ -18,9 +20,33 @@ function agregarAmigo(){
         }else{
             arrayAmigos.push(amigo);
         }
-        
+
         limpiarCampoEntrada();
         mostrarListaAmigos();
+    }
+}
+
+function sortearAmigo(){
+    let lengthArray = arrayAmigos.length;
+    let amigoSorteado = "";
+
+    if(lengthArray === 0){
+        alert("Agregue amigos, para comenzar el sorteo");
+        mostrarTextoEnElemento(resultadoAmigoSecreto, "");
+        return;
+    }else{
+        indiceAleatorio = generarIndiceAletorio(lengthArray);
+
+        for(let i = 0; i < lengthArray; i++){
+            if(indiceAleatorio == i){
+                amigoSorteado = arrayAmigos[i];
+                arrayAmigos.splice(indiceAleatorio,1);
+                mostrarListaAmigos();
+            }
+        }
+
+        mostrarTextoEnElemento(resultadoAmigoSecreto, "");
+        mostrarTextoEnElemento(resultadoAmigoSecreto, `El amigo secreto sorteado es ${amigoSorteado}`);
     }
 }
 
@@ -34,11 +60,19 @@ function limpiarEspacios(cadena){
 
 function mostrarListaAmigos(){
     let li = "";
-    listAmigo.innerHTML = "";
+    mostrarTextoEnElemento(listAmigo, "");
 
     for(let i = 0; i < arrayAmigos.length; i ++){
         li += `<li>${arrayAmigos[i]}</li>`;
     }
 
-    listAmigo.innerHTML = li;
+    mostrarTextoEnElemento(listAmigo, li);
+}
+
+function generarIndiceAletorio(lengthArray){
+    return Math.floor(Math.random()*lengthArray);
+}
+
+function mostrarTextoEnElemento(elemento, texto){
+    elemento.innerHTML = texto;
 }
