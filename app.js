9e5c25cd7f0174f71fa5013fbbf31abbb99868e1
iSmithmilla <jhonmilla1607@gmail.com>
2025-01-21 22:13:04 -0500

    2025.01.21 - Implementar funcion agregar amigos - 22:12pm

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let arrayAmigos = [];
+let inputAmigo = document.querySelector("#amigo");
+let amigo = "";
+
+
+function agregarAmigo(){
+    amigo = limpiarEspacios(inputAmigo.value);
+
+    if(amigo.length == 0 || amigo == ""){
+        alert("Porfavor, inserte un nombre");
+        return;
+    }else{
+        arrayAmigos.push(amigo);
+        limpiarCampoEntrada();
+    }
+}
+
+function limpiarCampoEntrada(){
+    inputAmigo.value = "";
+}
+
+function limpiarEspacios(cadena){
+    return cadena.trim();
+}
