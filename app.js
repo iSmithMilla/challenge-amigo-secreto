39b596b0926b3fcb31b5bb0f627da190dab935b6
iSmithmilla <jhonmilla1607@gmail.com>
2025-01-21 22:44:05 -0500

    2025.01.21 - Implementar funcion para actualizar lista de amigos - 22:43pm

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let arrayAmigos = [];
 let inputAmigo = document.querySelector("#amigo");
+let listAmigo = document.querySelector("#listaAmigos");
 let amigo = "";
 
 
@@ -11,8 +12,15 @@ function agregarAmigo(){
         alert("Porfavor, inserte un nombre");
         return;
     }else{
-        arrayAmigos.push(amigo);
+        
+        if(arrayAmigos.includes(amigo)){
+            alert("El amigo ingresado ya existe, ingrese uno diferente");
+        }else{
+            arrayAmigos.push(amigo);
+        }
+        
         limpiarCampoEntrada();
+        mostrarListaAmigos();
     }
 }
 
@@ -22,4 +30,15 @@ function limpiarCampoEntrada(){
 
 function limpiarEspacios(cadena){
     return cadena.trim();
+}
+
+function mostrarListaAmigos(){
+    let li = "";
+    listAmigo.innerHTML = "";
+
+    for(let i = 0; i < arrayAmigos.length; i ++){
+        li += `<li>${arrayAmigos[i]}</li>`;
+    }
+
+    listAmigo.innerHTML = li;
 }
